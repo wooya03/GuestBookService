8265aac3c1c48cedc6241e2ee4d746fc189e6bed
Wooya <wooya_0@naver.com>
2024-07-11 10:22:25 +0900

    .

--- src/main/java/kr/ac/ync/ex/exception/CustomException.java
+++ src/main/java/kr/ac/ync/ex/exception/CustomException.java
@@ -9,5 +9,4 @@ import org.springframework.http.HttpStatus;
 public class CustomException extends RuntimeException {
     private final HttpStatus httpStatus;
     private final String message;
-
 }
---
+++ src/main/java/kr/ac/ync/ex/exception/controller/GuestBookController.java
@@ -0,0 +1,16 @@
+package kr.ac.ync.ex.exception.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/guestbook")
+@Slf4j
+public class GuestBookController {
+    @GetMapping("")
+    public
+
+    @GetMapping("/list")
+}
